718efca7f9ce2a8a5c21a194dfc50d64ae8549ee
Zackjw70 <zjwyeth@email.neit.edu>
2025-01-10 16:45:04 -0500

    Finished Week 1 Assignment

--- Demos/Week1Demo/app.js
+++ Demos/Week1Demo/app.js
@@ -6,13 +6,22 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res)=>{
+app.get("/index", (req, res)=>{
     res.sendFile(path.join(__dirname,"public", "index.html"));
 });
 
-app.get("/json", (req, res)=>{
+app.get("/todo", (req, res)=>{
     res.sendFile(path.join(__dirname,"public", "players.json"));
 });
+
+app.get("/read-todo", (req, res)=>{
+    res.sendFile(path.join(__dirname,"public", "todo.html"));
+});
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname,"public", "index.html"));
+});
+
 
 app.listen(PORT, ()=>{
     console.log("Server Running on Port 3000")
